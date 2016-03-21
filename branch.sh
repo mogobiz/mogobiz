235024902ab6affccd32a2fe9459293d83be8e08
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2016-03-21 22:00:07 +0100

    branch submodules

---
+++ branch.sh
@@ -0,0 +1,37 @@
+cd "$(dirname "$0")"
+
+if [ "$#" != "2" ]; then
+	echo "Usage: branch.sh remove-name branch"
+	echo "remote-name : usually origin"
+	echo "branch : local & remote branch name"
+	exit 1
+fi
+
+echo "$1 $2"
+git checkout -b $2
+git checkout $2
+git push $1 $2
+
+createbranch() {
+	submodules=`ls -d mogo*`
+	git checkout -b $2
+	git checkout $2
+	git push $1 $2
+	for submodule in $submodules
+	do
+		echo "$submodule"
+		cd $submodule
+		git checkout -b $2
+		git checkout $2
+		git push $1 $2
+		cd ..
+	done
+}
+
+cd grails
+createbranch $1 $2
+cd ..
+cd scala
+createbranch $1 $2
+cd ..
+
