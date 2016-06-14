8e4edaf4a8e5057ca9e562bcc6b7e86712005a4c
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2016-06-14 21:45:52 +0200

    Initial migration

---
+++ current.sh
@@ -0,0 +1,22 @@
+cd "$(dirname "$0")"
+
+
+current() {
+        submodules=`ls -d mogo*/`
+        git rev-parse --abbrev-ref HEAD
+        for submodule in $submodules
+        do
+                cd $submodule
+                branch=`git rev-parse --abbrev-ref HEAD`
+                echo "$branch $submodule"
+                cd ..
+        done
+}
+
+cd grails
+current
+cd ..
+cd scala
+current
+cd ..
+
