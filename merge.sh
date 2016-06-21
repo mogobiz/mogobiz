46f4941f0cc38da56fb026c1ecf48c6b17779472
Stephane Manciot <stephane.manciot@ebiznext.com>
2016-06-21 09:39:11 +0200

    add script to merge recursively from a source to a destination branche name

---
+++ merge.sh
@@ -0,0 +1,39 @@
+cd "$(dirname "$0")"
+
+if [ "$#" != "2" ]; then
+	echo "Usage: merge.sh source dest"
+	echo "merge : source & dest branch name"
+	exit 1
+fi
+
+export source=$1
+export dest=$2
+
+echo "$source $dest"
+git checkout $dest
+#git push $dest
+git merge -m "Merge branch $source into $dest" --no-ff $source		
+
+mergebranch() {
+	submodules=`ls -d mogo*/`
+	git checkout $dest
+#	git push $dest
+    git merge -m "Merge branch $source into $dest" --no-ff $source		
+	for submodule in $submodules
+	do
+		echo "$submodule"
+		cd $submodule
+        git checkout -b $dest
+		git checkout $dest
+#		git push $dest
+        git merge -m "Merge branch master into develop" --no-ff master		
+		cd ..
+	done
+}
+
+cd grails
+mergebranch
+cd ..
+cd scala
+mergebranch
+cd ..
