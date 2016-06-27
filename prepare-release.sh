2863547515df381554ed127fb5d31a043d146bfc
Stephane Manciot <stephane.manciot@ebiznext.com>
2016-06-27 20:23:59 +0200

    add prepare release

---
+++ prepare-release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+export RELEASE_VERSION=$1
+
+preparerelease() {
+	submodules=`ls -d mogo*/`
+	for submodule in $submodules
+	do
+		echo "$submodule"
+		cd $submodule
+		git add .
+#        git commit -a -m "prepare release ${RELEASE_VERSION}"
+#		git push
+		cd ..
+	done
+}
+
+cd grails
+preparerelease
+cd ..
+#cd scala
+#preparerelease
+#cd ..
+
+git add .
+#git commit -a -m "prepare release ${RELEASE_VERSION}"
+#git push
